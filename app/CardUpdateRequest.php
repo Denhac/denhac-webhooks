54aa79d4fc6ba68c295e0b287ef00af966b747b7
Justin Nesselrotte <admin@jnesselr.org>
2020-03-19 21:43:12 -0600

    Fixing naming for a status

--- app/CardUpdateRequest.php
+++ app/CardUpdateRequest.php
@@ -16,7 +16,7 @@ class CardUpdateRequest extends Model
     public const ACTIVATION_TYPE = 'enable';
     public const DEACTIVATION_TYPE = 'disable';
 
-    public const STATUS_NOT_DONE = 'not_dene';
+    public const STATUS_NOT_DONE = 'not_done';
     public const STATUS_SUCCESS = 'success';
     public const STATUS_ERROR_MULTIPLE_CARD_HOLDERS = 'error_multiple_card_holders';
     public const STATUS_DEACTIVATE_CARD_NOT_FOUND = 'deactivate_card_not_found';
