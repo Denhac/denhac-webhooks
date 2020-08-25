d1d7ab7015922639c350a202cdc9499d0a1b61b4
Justin Nesselrotte <admin@jnesselr.org>
2020-08-25 00:13:35 -0600

    Added test case for "No Event Test User"

---
+++ tests/Unit/Aggregates/MembershipAggregate/NoEventUserTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit\Aggregates\MembershipAggregate;
+
+
+use App\Aggregates\MembershipAggregate;
+use App\StorableEvents\CustomerIsNoEventTestUser;
+use Illuminate\Support\Facades\Event;
+use Spatie\EventSourcing\Facades\Projectionist;
+use Tests\TestCase;
+
+class NoEventUserTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Event::fake();
+        Projectionist::withoutEventHandlers();
+    }
+
+    /** @test */
+    public function create_customer_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->createCustomer($customer)
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function update_customer_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->updateCustomer($customer)
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function delete_customer_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->deleteCustomer($customer)
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function import_customer_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->importCustomer($customer)
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function create_subscription_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->createSubscription($this->subscription())
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function update_subscription_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->updateSubscription($this->subscription())
+            ->assertNothingRecorded();
+    }
+
+    /** @test */
+    public function import_subscription_call_does_not_make_event()
+    {
+        $customer = $this->customer();
+
+        MembershipAggregate::fakeCustomer($customer)
+            ->given(new CustomerIsNoEventTestUser($customer->id))
+            ->importSubscription($this->subscription())
+            ->assertNothingRecorded();
+    }
+}
