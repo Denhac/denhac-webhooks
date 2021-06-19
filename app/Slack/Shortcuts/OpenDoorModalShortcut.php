4e3094c65bec7f373314ca604dff21ce59e5b93c
Justin Nesselrotte <admin@jnesselr.org>
2021-06-19 12:54:49 -0600

    Fixing open door modal shortcut

--- app/Slack/Shortcuts/OpenDoorModalShortcut.php
+++ app/Slack/Shortcuts/OpenDoorModalShortcut.php
@@ -5,6 +5,7 @@ namespace App\Slack\Shortcuts;
 
 use App\Events\DoorControlUpdated;
 use App\Http\Requests\SlackRequest;
+use App\Slack\Modals\OpenDoorModal;
 use App\WinDSX\Door;
 
 class OpenDoorModalShortcut implements ShortcutInterface
@@ -16,11 +17,8 @@ class OpenDoorModalShortcut implements ShortcutInterface
 
     public static function handle(SlackRequest $request)
     {
-        $customer = $request->customer();
-
         // TODO Verify if they're at the space and challenge if not
-        if ($customer->hasCapability('denhac_can_verify_member_id')) {
-            event(new DoorControlUpdated(5, Door::kitchenGlassDoor()));
-        }
+        $modal = new OpenDoorModal();
+        $modal->open($request->get('trigger_id'));
     }
 }
