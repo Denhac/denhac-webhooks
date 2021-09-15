4f9f94d5bb9debbc7640606b6f34ba46dc5e5dc0
Justin Nesselrotte <admin@jnesselr.org>
2021-09-15 00:28:22 -0600

    Added trainable equipment table

---
+++ app/TrainableEquipment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrainableEquipment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'user_plan_id',
+        'user_slack_id',
+        'user_email',
+        'trainer_plan_id',
+        'trainer_slack_id',
+        'trainer_email',
+    ];
+}
---
+++ database/migrations/2021_09_15_062517_create_trainable_equipment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainableEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainable_equipment', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('user_plan_id');
+            $table->string('user_slack_id')->nullable();
+            $table->string('user_email')->nullable();
+            $table->integer('trainer_plan_id');
+            $table->string('trainer_slack_id')->nullable();
+            $table->string('trainer_email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainable_equipment');
+    }
+}
