15e36c12ff5b082b6ae6d427225b65d245211277
Justin Nesselrotte <admin@jnesselr.org>
2023-08-05 03:27:04 -0600

    Switching from master to main branch

--- deploy/server_deploy.sh
+++ deploy/server_deploy.sh
@@ -7,8 +7,8 @@ echo "Deploying application ..."
 php artisan down
 
 # Update codebase
-git fetch origin master
-git reset --hard origin/master
+git fetch origin main
+git reset --hard origin/main
 
 # Install dependencies based on lock file
 composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
