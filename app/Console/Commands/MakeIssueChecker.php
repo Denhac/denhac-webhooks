f71049b2f881a37906d39c48083e8e6e83234597
Justin Nesselrotte <admin@jnesselr.org>
2023-08-07 02:09:15 -0600

    Added generator for issue checker

---
+++ app/Console/Commands/MakeIssueChecker.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+class MakeIssueChecker extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:issue-checker {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a new issue checker class';
+
+    protected $type = "Issue Checker";
+
+    protected function getStub()
+    {
+        return app_path('Console/Stubs/make-issue-checker.stub');
+    }
+}
