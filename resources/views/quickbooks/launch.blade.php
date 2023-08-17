ab68ce469aa6e812b93b5b48893e5b83037cec22
Justin Nesselrotte <admin@jnesselr.org>
2023-08-17 16:07:20 -0600

    Fixed auth url variable

--- resources/views/quickbooks/launch.blade.php
+++ resources/views/quickbooks/launch.blade.php
@@ -1,1 +1,1 @@
-This is the launch page for QuickBooks. <a href="{{ $authUrl }}">Click Here</a> to start the launch process.
+This is the launch page for QuickBooks. <a href="{{ $auth_url }}">Click Here</a> to start the launch process.
