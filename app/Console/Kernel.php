6432e62653bb9dad1154140479e01f680b37fe54
Justin Nesselrotte <admin@jnesselr.org>
2023-08-23 23:04:48 -0600

    Added cron job for generating vending net journal entry

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Actions\QuickBooks\GenerateVendingNetJournalEntry;
 use App\Aggregates\CardNotifierAggregate;
 use App\Console\Commands\IdentifyIssues;
 use App\Console\Commands\LinkQuickbooks;
@@ -12,6 +13,7 @@ use App\Console\Commands\SetUpDenhacWebhooks;
 use App\Console\Commands\SlackProfileFieldsUpdate;
 use App\Console\Commands\UpdateBaseData;
 use App\External\QuickBooks\QuickBooksAuthSettings;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use QuickBooksOnline\API\Core\OAuth\OAuth2\OAuth2LoginHelper;
@@ -46,14 +48,20 @@ class Kernel extends ConsoleKernel
             ->call(function () {
                 CardNotifierAggregate::make()->sendNotificationEmail()->persist();
             })
-            ->weeklyOn(6, '13:00');
+            ->weeklyOn(Schedule::SATURDAY, '13:00');
 
         $schedule->command('denhac:slack-profile-fields-update')
             ->daily();
 
         $schedule->command('passport:purge')->hourly();
 
+        // QuickBooks tokens expire every hour. Every half should prevent any issues with a job running right as a token expires.
         $schedule->call(fn() => $this->refreshQuickBooksAccessToken())->everyThirtyMinutes();
+
+        // daily at noon because te cron is in UTC but I grab Denver timezone minus one day. This makes the date string
+        // for searching orders as well as the date used for the QuickBooks entry correct regardless of if it's daylight
+        //savings time or not.
+        $schedule->call(fn() => $this->generateVendingNetJournalEntry())->dailyAt('12:00');
     }
 
     protected function refreshQuickBooksAccessToken(): void
@@ -62,6 +70,17 @@ class Kernel extends ConsoleKernel
             return;
         }
         app(OAuth2LoginHelper::class);  // This should refresh the token automatically on resolving
+    }
+
+    protected function generateVendingNetJournalEntry(): void
+    {
+        if (!QuickBooksAuthSettings::hasKnownAuth()) {
+            return;
+        }
+        $yesterday = Carbon::now('America/Denver')->subDay();
+        app(GenerateVendingNetJournalEntry::class)
+            ->onQueue()
+            ->execute($yesterday);
     }
 
     /**
