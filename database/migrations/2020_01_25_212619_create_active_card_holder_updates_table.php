62858ca82d4d64f378527aef8076722fdddbf6a0
Shift <shift@laravelshift.com>
2023-12-04 19:23:53 +0000

    Adopt anonymous migrations

--- database/migrations/2020_01_25_212619_create_active_card_holder_updates_table.php
+++ database/migrations/2020_01_25_212619_create_active_card_holder_updates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateActiveCardHolderUpdatesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateActiveCardHolderUpdatesTable extends Migration
     {
         Schema::dropIfExists('active_card_holder_updates');
     }
-}
+};
