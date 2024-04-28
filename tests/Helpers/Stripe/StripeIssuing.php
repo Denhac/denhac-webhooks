91355def4943ee0d369c2cdc8741e3c36228d228
Justin Nesselrotte <admin@jnesselr.org>
2024-04-28 11:57:06 -0600

    Refactored Stripe Id Generator to its own class

--- tests/Helpers/Stripe/StripeIssuing.php
+++ tests/Helpers/Stripe/StripeIssuing.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Helpers\Stripe;
 
+use App\External\Stripe\StripeIdGenerator;
 use Illuminate\Foundation\Testing\WithFaker;
 use Stripe\Issuing\Card;
 use Stripe\Issuing\Cardholder;
@@ -12,18 +13,9 @@ trait StripeIssuing
 {
     use WithFaker;
 
-    private const ID_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
-
-    public function stripeId($prefix): string
-    {
-        return $prefix . '_' . implode('', $this->faker->randomElements(self::ID_CHARACTERS, 24, true));
-    }
-
     public function stripeIssuingCard(): Card
     {
-        $card = new Card($this->stripeId('ic'));
+        $card = new Card(StripeIdGenerator::make('ic'));
         $card->cardholder = $this->stripeIssuingCardHolder();
         $card->status = $this->faker->randomElement(['active', 'cancelled', 'inactive']);
         $card->type = $this->faker->randomElement(['physical', 'virtual']);
@@ -33,7 +25,7 @@ trait StripeIssuing
 
     public function stripeIssuingCardHolder(): Cardholder
     {
-        $cardHolder = new Cardholder($this->stripeId('ich'));
+        $cardHolder = new Cardholder(StripeIdGenerator::make('ich'));
         $cardHolder->phone_number = $this->faker->phoneNumber();
 
         return $cardHolder;
