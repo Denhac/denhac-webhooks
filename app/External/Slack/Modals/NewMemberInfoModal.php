aa4ed36906cb671ceb2c84ac455516b0d712fb61
Eddie Zaneski <eddiezane@gmail.com>
2024-09-11 12:03:59 -0600

    refactor: update new member info (#89)
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- app/External/Slack/Modals/NewMemberInfoModal.php
+++ app/External/Slack/Modals/NewMemberInfoModal.php
@@ -12,6 +12,16 @@ class NewMemberInfoModal implements ModalInterface
 
     private Modal $modalView;
 
+    private const INFO = array(
+        "Don't live or sleep at the space. That includes in the parking lot. Don't leave your car in the lot overnight.",
+        "We're a drug and alcohol free space.",
+        "There are other tenants in the building. Don't go upstairs except for the gender neutral bathroom since there's not one on the first floor. Close denhac doors if no one else is in the room. The exterior doors are unlocked randomly throughout the day.",
+        "Slack has many channels you can join. Channels prefixed with help- are a good place to start to ask for training on equipment. Reply in threads to make channels easier to follow.",
+        "Read all signs everywhere and abide by them. Also read the wiki. If something isn't in the wiki, add it.",
+        "The last Saturday of the month is Hack denhac Day. The space is closed for personal project work. Come help.",
+        "This is a community. You are not a customer, you are a member. Leave the space better than you found it. Take out the trash. Communicate any issues through Slack.",
+    );
+
     public function __construct()
     {
         $this->modalView = Kit::newModal()
@@ -20,16 +30,10 @@ class NewMemberInfoModal implements ModalInterface
             ->clearOnClose(true)
             ->clearOnClose('Close');
 
-        $this->modalView->newSection()
-            ->mrkdwnText("1. Don't live at/sleep at the space. That includes in the parking lot. Don't leave your car in the lot overnight.");
-        $this->modalView->newSection()
-            ->mrkdwnText("2. We're a drug and alcohol free space.");
-        $this->modalView->newSection()
-            ->mrkdwnText("3. There are other tenants in the building. Don't go upstairs except for the gender neutral bathroom since there's not one on the first floor. Close denhac doors if no one else is in the room.");
-        $this->modalView->newSection()
-            ->mrkdwnText("4. Our Slack instance has many channels, you'll only be in a few by default. Channel prefixes like help- are a good place to start to ask for training on equipment.");
-        $this->modalView->newSection()
-            ->mrkdwnText('5. Leave the space better than you found it. Communicate any issues through Slack.');
+        foreach ($this->INFO as $info) {
+            $this->modalView->newSection()
+                ->mrkdwnText($info);
+        }
     }
 
     public static function callbackId(): string
