afae05eb15d3ca0f57613a9da7b8f1d635295000
Anne DeCusatis <anne-decusatis@users.noreply.github.com>
2024-12-10 18:08:28 -0700

    Add anti-harassment info as approved by board on 2024-11-05

--- app/External/Slack/Modals/NewMemberInfoModal.php
+++ app/External/Slack/Modals/NewMemberInfoModal.php
@@ -13,12 +13,13 @@ class NewMemberInfoModal implements ModalInterface
     private Modal $modalView;
 
     private const INFO = array(
-        "This is a community. You are a member, not a customer. This organization runs on the contributions of it's members.",
+        "This is a community. You are a member, not a customer. This organization runs on the contributions of its members.",
         "We're a drug and alcohol free space. There is hazardous equipment here, always keep everyone's safety in mind.",
         "Don't live or sleep at the space. That includes in the parking lot. Don't leave your car in the lot overnight.",
         "There are other tenants in the building. Don't go upstairs except for the gender neutral bathroom since there's not one on the first floor. Close denhac doors if no one else is in the room. The exterior doors are unlocked randomly throughout the day.",
         "Slack has many channels you can join. Channels prefixed with help- are a good place to start to ask for training on equipment. Channels prefixed with sig- are a good place to start volunteering and get involved. Reply in threads to make channels easier to follow.",
         "Read all signs everywhere and abide by them. Also read the wiki. If something isn't in the wiki, add it. Check out the How To page on our website, it provides some information on 'How to be a denhac member' essentially.",
+        "No Harassment or Bullying: This includes, but is not limited to, attention that comes after a request to stop. Recognize and respect everyone's differences. Treat all members with kindness and respect. Our anti-harassment policy is part of our member agreement - please read it."
         "The last Saturday of the month is Hack denhac Day. The space is closed for personal project work. Come help us work on the space.",
         "Leave the space better than you found it. Take out the trash. Communicate any issues through Slack.",
     );
