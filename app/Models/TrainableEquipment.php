897cd03f2adc9751756ab6685c3c921115ca6165
Justin Nesselrotte <admin@jnesselr.org>
2025-02-24 11:14:39 -0700

    Added soft deletes to trainable equipment

--- app/Models/TrainableEquipment.php
+++ app/Models/TrainableEquipment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int id
@@ -14,6 +15,7 @@ use Illuminate\Database\Eloquent\Model;
 class TrainableEquipment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
