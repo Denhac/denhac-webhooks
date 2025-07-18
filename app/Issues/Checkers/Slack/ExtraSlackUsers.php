11903bafb79a7dcac4c4fe875f9a8ae9f41ee099
Justin Nesselrotte <admin@jnesselr.org>
2025-07-18 11:43:02 -0600

    Better handling for Slack user without slack id link

--- app/Issues/Checkers/Slack/ExtraSlackUsers.php
+++ app/Issues/Checkers/Slack/ExtraSlackUsers.php
@@ -11,6 +11,7 @@ use App\Issues\Checkers\SlackMembershipHelper;
 use App\Issues\Types\Slack\FullUserNoRecord;
 use App\Issues\Types\Slack\MemberHasRestrictedAccount;
 use App\Issues\Types\Slack\NonMemberHasFullAccount;
+use App\Issues\Types\Slack\UserMissingSlackLink;
 
 class ExtraSlackUsers implements IssueCheck
 {
@@ -19,8 +20,10 @@ class ExtraSlackUsers implements IssueCheck
 
     public function __construct(
         private readonly AggregateCustomerData $aggregateCustomerData,
-        private readonly SlackUsers $slackUsers
-    ) {}
+        private readonly SlackUsers            $slackUsers
+    )
+    {
+    }
 
     public function generateIssues(): void
     {
@@ -42,39 +45,58 @@ class ExtraSlackUsers implements IssueCheck
 
         $members = $this->aggregateCustomerData->get();
 
-        $slackUsers
-            ->each(function ($user) use ($members) {
-                $membersForSlackId = $members
-                    ->filter(function ($member) use ($user) {
-                        /** @var MemberData $member */
-                        return $member->slackId == $user['id'];
-                    });
+        $slackUsers->each(fn($user) => $this->handleSlackUser($user, $members));
+    }
 
-                if ($membersForSlackId->count() == 0) {  // TODO Check for multi/single channel guests outside of public as well
-                    if ($this->isFullSlackUser($user)) {
-                        $this->issues->add(new FullUserNoRecord($user));
-                    }
-
-                    return;
-                }
-
+    private function handleSlackUser($user, $members): void
+    {
+        $membersForSlackId = $members
+            ->filter(function ($member) use ($user) {
                 /** @var MemberData $member */
-                $member = $membersForSlackId->first();
-
-                if ($member->isMember) {
-                    if (array_key_exists('is_invited_user', $user) && $user['is_invited_user']) {
-                        return; // Do nothing, we've sent the invite and that's all we can do.
-                    } elseif (array_key_exists('deleted', $user) && $user['deleted']) {
-                        $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'deleted'));
-                    } elseif (array_key_exists('is_restricted', $user) && $user['is_restricted']) {
-                        $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'a multi-channel guest'));
-                    } elseif (array_key_exists('is_ultra_restricted', $user) && $user['is_ultra_restricted']) {
-                        $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'a single-channel guest'));
-
-                    }
-                } elseif ($this->isFullSlackUser($user)) {  // TODO Check for multi/single channel guests outside of public as well
-                    $this->issues->add(new NonMemberHasFullAccount($member, $user));
-                }
+                return $member->slackId == $user['id'];
             });
+
+        if ($membersForSlackId->count() == 0) {  // TODO Check for multi/single channel guests outside of public as well
+            $this->handleNoMatchingAccount($user, $members);
+
+            return;
+        }
+
+        /** @var MemberData $member */
+        $member = $membersForSlackId->first();
+
+        if ($member->isMember) {
+            if (array_key_exists('is_invited_user', $user) && $user['is_invited_user']) {
+                return; // Do nothing, we've sent the invite and that's all we can do.
+            } elseif (array_key_exists('deleted', $user) && $user['deleted']) {
+                $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'deleted'));
+            } elseif (array_key_exists('is_restricted', $user) && $user['is_restricted']) {
+                $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'a multi-channel guest'));
+            } elseif (array_key_exists('is_ultra_restricted', $user) && $user['is_ultra_restricted']) {
+                $this->issues->add(new MemberHasRestrictedAccount($member, $user, 'a single-channel guest'));
+
+            }
+        } elseif ($this->isFullSlackUser($user)) {  // TODO Check for multi/single channel guests outside of public as well
+            $this->issues->add(new NonMemberHasFullAccount($member, $user));
+        }
+    }
+
+    private function handleNoMatchingAccount($user, $members): void
+    {
+        $membersForEmail = $members
+            ->filter(function ($member) use ($user) {
+                /** @var MemberData $member */
+                return in_array($user['email'], $member->emails->all());
+            });
+
+        if($membersForEmail->count() > 0) {
+            $this->issues->add(new UserMissingSlackLink($user, $membersForEmail));
+
+            return;
+        }
+
+        if ($this->isFullSlackUser($user)) {
+            $this->issues->add(new FullUserNoRecord($user));
+        }
     }
 }
---
+++ app/Issues/Types/Slack/UserMissingSlackLink.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Issues\Types\Slack;
+
+use App\DataCache\MemberData;
+use App\External\WooCommerce\Api\WooCommerceApi;
+use App\Issues\Types\ICanFixThem;
+use App\Issues\Types\IssueBase;
+
+class UserMissingSlackLink extends IssueBase
+{
+    use ICanFixThem;
+
+    public function __construct(
+        private $slackUser,
+        private $membersForEmail,
+    )
+    {
+    }
+
+    public static function getIssueNumber(): int
+    {
+        return 304;  // auto-generated based on namespace and existing issues
+    }
+
+    public static function getIssueTitle(): string
+    {
+        return "Slack: User missing slack link";
+    }
+
+    public function getIssueText(): string
+    {
+        $members = $this->membersForEmail
+            ->map(fn ($m) => "{$m->first_name} {$m->last_name} ($m->id)")
+            ->implode(', ');
+
+        return "{$this->slackUser['name']} with slack id ({$this->slackUser['id']}) probably belongs to one of these users: $members";
+    }
+
+    public function fix(): bool
+    {
+        $choices = $this->issueFixChoice();
+
+        foreach ($this->membersForEmail as $m) {
+            $choices->option("{$m->first_name} {$m->last_name} ($m->id)", fn () => $this->assignToMember($m));
+        }
+
+        return $choices->run();
+    }
+
+    private function assignToMember(MemberData $member): true
+    {
+        $wooCommerceApi = app(WooCommerceApi::class);
+
+        $wooCommerceApi->customers
+            ->update($member->id, [
+                'meta_data' => [
+                    [
+                        'key' => 'access_slack_id',
+                        'value' => $this->slackUser['id'],
+                    ],
+                ],
+            ]);
+
+        return true;
+    }
+}
