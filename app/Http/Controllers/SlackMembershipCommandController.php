823a9ba3af4eb423a4bab955d5434f494b441d38
Casey Hungler <cmhungler@gmail.com>
2025-10-04 00:01:27 +0000

    Document slack dev onboarding

--- app/Http/Controllers/SlackMembershipCommandController.php
+++ app/Http/Controllers/SlackMembershipCommandController.php
@@ -6,14 +6,15 @@ use App\External\Slack\CommonResponses;
 use App\External\Slack\Modals\MembershipOptionsModal;
 use App\Http\Requests\SlackRequest;
 use SlackPhp\BlockKit\Kit;
+use Illuminate\Support\Facades\Log;
 
 class SlackMembershipCommandController extends Controller
 {
     public function __invoke(SlackRequest $request)
     {
         $customer = $request->customer();
-
         if ($customer === null) {
+            Log::info("Membership command invoked by unknown user {$request->getSlackId()}");
             return Kit::message(
                 text: CommonResponses::unrecognizedUser(),
             );
