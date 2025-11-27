3c281b144f6ce2d77cd42314dd6125e55df9d04d
Justin Nesselrotte <admin@jnesselr.org>
2025-11-27 15:08:08 -0700

    Improved Card Scanned Controller readability and fixed some bugs

--- app/Http/Controllers/CardScannedController.php
+++ app/Http/Controllers/CardScannedController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\External\WinDSX\Door;
 use App\Models\Card;
+use App\Models\Customer;
 use App\Models\Waiver;
 use App\Notifications\CardAccessAllowedButNotAMemberRedAlert;
 use App\Notifications\CardAccessDeniedBadDoor;
@@ -41,9 +42,6 @@ class CardScannedController extends Controller
             return;
         }
 
-        $accessAllowed = $request->get('access_allowed');
-        $device = $request->get('device');
-
         /** @var Card $card */
         $card = $cards->first();
         if (is_null($card)) {
@@ -52,9 +50,6 @@ class CardScannedController extends Controller
             return;
         }
 
-        $updatedAt = $card->updated_at;
-        $updatedAt->tz('America/Denver');
-        $scanTime = Carbon::parse($request->json('scan_time'), 'America/Denver');
         $customer = $card->customer;
 
         if (is_null($customer)) {
@@ -64,73 +59,111 @@ class CardScannedController extends Controller
         }
 
         if ($customer->member) {
-            $this->log->info("They're a member!");
-            if (! $accessAllowed) {
-                $this->log->info('No access though.');
-                // They weren't given access
-
-                $door = Door::byDSXDeviceId($device);
-
-                if (is_null($door)) {
-                    $this->log->info("We don't know about this door {$device}");
-
-                    // We don't know about this door. Do nothing
-                    return;
-                } elseif ($door->membersCanBadgeIn) {
-                    if (! $customer->hasSignedMembershipWaiver()) {
-                        $notification = new CardAccessDeniedNoWaiver($customer);
-
-                        Notification::route('mail', $customer->email)
-                            ->notify($notification);
-
-                        return;
-                    }
-
-                    /** @var Waiver $waiver */
-                    $waiver = $customer->getMembershipWaiver();
-
-                    if ($scanTime->subMinutes(10) < $waiver->updated_at) {
-                        return;  // They just signed the waiver, give it a bit to activate their card before emailing everyone
-                    }
-
-                    if ($scanTime->subMinutes(10) < $updatedAt) {
-                        // We created this card less than 10 minutes ago. It might not even be active yet.
-                        return;
-                    }
-
-                    $notification = new CardAccessDeniedButWereWorkingOnIt(
-                        $request->json('first_name'),
-                        $request->json('last_name'),
-                        $request->json('card_num'),
-                        $request->json('scan_time')
-                    );
-
-                    Notification::route('mail', $customer->email)
-                        ->notify($notification);
-                } else {
-                    $notification = new CardAccessDeniedBadDoor;
-
-                    Notification::route('mail', $customer->email)
-                        ->notify($notification);
-                }
-            }
+            $this->handleMemberBadge($request, $customer, $card);
         } else {
-            if ($accessAllowed) {
-                $notification = new CardAccessAllowedButNotAMemberRedAlert(
-                    $request->json('first_name'),
-                    $request->json('last_name'),
-                    $request->json('card_num'),
-                    $request->json('scan_time')
-                );
+            $this->handleNonMemberBadgeIn($request, $customer);
+        }
+    }
 
-                Notification::route('mail', config('denhac.access_email'))
-                    ->notify($notification);
-            } else {
-                $notification = new CardAccessDeniedBecauseNotAMember;
+    /**
+     * @param Request $request
+     * @param Customer $customer
+     * @param Card $card
+     * @return void
+     */
+    public function handleMemberBadge(Request $request, Customer $customer, Card $card): void
+    {
+        $accessAllowed = $request->get('access_allowed');
+        $device = $request->get('device');
+
+        $scanTime = Carbon::parse($request->json('scan_time'), 'America/Denver');
+
+        if ($accessAllowed) {
+            // They're a member and access was allowed. Business as usual.
+            return;
+        }
+
+        $door = Door::byDSXDeviceId($device);
+
+        if (is_null($door)) {
+            // We don't know about this door. Probably another tenant door. Do nothing.
+            return;
+        }
+
+        if ($door->membersCanBadgeIn) {
+            // They must have signed the waiver first
+            if (! $customer->hasSignedMembershipWaiver()) {
+                $this->log->info("Member access not allowed as no waiver is signed.");
+
+                $notification = new CardAccessDeniedNoWaiver($customer);
 
                 Notification::route('mail', $customer->email)
                     ->notify($notification);
+
+                return;
             }
+
+            /** @var Waiver $waiver */
+            $waiver = $customer->getMembershipWaiver();
+
+            // The system has seen the signed waiver, but the card access computer may not have updated yet.
+            if ($scanTime->subMinutes(10) < $waiver->updated_at->tz('America/Denver')) {
+                $this->log->info("Member access not allowed as waiver was signed recently.");
+
+                return;
+            }
+
+            // The system recently updated this card model, so it's possible it was just issued. There are other reasons
+            // the card could get updated, but most of them relate to activation/deactivation so it's a good approximation.
+            if ($scanTime->subMinutes(10) < $card->updated_at->tz('America/Denver')) {
+                $this->log->info("Member access not allowed as card was updated recently.");
+
+                return;
+            }
+
+            $this->log->info("Member access not allowed for some unknown reason.");
+
+            $notification = new CardAccessDeniedButWereWorkingOnIt(
+                $request->json('first_name'),
+                $request->json('last_name'),
+                $request->json('card_num'),
+                $request->json('scan_time')
+            );
+
+        } else {
+            // This currently only happens because there's one door with badge access that leads into denhac space, but
+            // kept not being safely secured and now is exit only.
+            $notification = new CardAccessDeniedBadDoor;
+        }
+
+        Notification::route('mail', $customer->email)
+            ->notify($notification);
+    }
+
+    /**
+     * @param Request $request
+     * @param Customer $customer
+     * @return void
+     */
+    public function handleNonMemberBadgeIn(Request $request, Customer $customer): void
+    {
+        $accessAllowed = $request->get('access_allowed');
+
+        if ($accessAllowed) {
+            $notification = new CardAccessAllowedButNotAMemberRedAlert(
+                $request->json('first_name'),
+                $request->json('last_name'),
+                $request->json('card_num'),
+                $request->json('scan_time')
+            );
+
+            Notification::route('mail', config('denhac.access_email'))
+                ->notify($notification);
+        } else {
+            $notification = new CardAccessDeniedBecauseNotAMember;
+
+            Notification::route('mail', $customer->email)
+                ->notify($notification);
         }
     }
 }
